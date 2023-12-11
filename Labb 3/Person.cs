e5fb3ab57c6fddbb9b541e3d577c924abc3c09f1
alexs <alexs@LAPTOP-CTBNBDVP>
2023-12-11 15:15:54 +0100

    Add project files.

---
+++ Labb 3/Person.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Labb_3
+{
+    public class Person
+    {
+        public string FirstName { get; private set; }
+        public string LastName{ get; private set; }
+        public Gender Gender { get; private set; }
+        public Hair Hair { get; private set; }
+        public DateTime Birthday { get; private set; }
+        public string EyeColour { get; private set; }
+
+        public Person (string firstname, string lastname, Gender gender, Hair hair, DateTime birthday, string eyecolour)
+        {
+            FirstName = firstname;
+            LastName = lastname;
+            Gender = gender;
+            Hair = hair;
+            EyeColour = eyecolour;
+            Birthday = birthday;
+            
+
+
+        }
+
+        public override string ToString()
+        {
+            return 
+            $"\nName:          {FirstName} {LastName}" +
+            $"\nDate of birth: {Birthday:yyyy/MM/dd}" +
+            $"\nGender:        {Gender}" +
+            $"\nHair:          {Hair.HairColour}  {Hair.HairLength}" +
+            $"\nEyecolour:     {EyeColour}"; 
+        }
+
+    }
+}
---
+++ Labb 3/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Labb_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstname = "Ahmad ";
+            string lastname = "Yazbek ";
+            Gender gender = Gender.Man;
+            Hair hair = new Hair { HairColour = "svart " + "och ", HairLength = "kort " };
+            DateTime birthday = new DateTime(2003, 08, 10);
+            string EyeColour = "brun ";
+
+            Person person = new Person(firstname, lastname, gender, hair, birthday, EyeColour);
+            
+            Console.WriteLine(person.ToString());
+            
+
+        }
+        
+
+    }
+}
